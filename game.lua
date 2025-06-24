809afa3ce5d785b153ee8bb6e00cc4d88f0fdedb
bawiiman <rockyman0504@naver.com>
2025-06-24 19:49:12 +0900

    [FEAT] 베이직 게임 스트럭쳐

--- game.lua
+++ game.lua
@@ -2,9 +2,12 @@ local Game = {}
 
 
 function Game:load()
+    -- Load Game Graphics.
     self.background_image = love.graphics.newImage( "images/background.png")
 end
 
 function Game:draw()
     love.graphics.draw(self.background_image)
 end
+
+return Game
